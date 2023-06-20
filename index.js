9cdf4ef7c8c4250403475d94eba450933b08a208
sanspoof <alex.spencer@live.com>
2023-06-20 17:17:12 +0100

    Now mimics the folder structure of the src and replicates in dist

--- index.js
+++ index.js
@@ -1,34 +1,93 @@
 const fs = require('fs');
+
 const path = require('path');
+
 const sharp = require('sharp');
 
-// changes these based on directory structure..
-const inputDirectory = './src/img';
-const outputDirectory = './dist/img';
+// Change these based on directory structure..
+// Althought keeping src and dist is a good idea
 
-fs.readdir(inputDirectory, (err, files) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(files);
-  
-// change the extensions below based on your use case...
-  
-  files.forEach((file) => {
-    // Check if the file is an image, might need adjusting for different image types...?
-    if (file.endsWith('.jpg') || file.endsWith('.jpeg') || file.endsWith('.png')) {
-      const inputFilePath = path.join(inputDirectory, file);
-      const outputFilePath = path.join(outputDirectory, `${file.slice(0, -4)}.jpg`);
-      sharp(inputFilePath)
-        .jpeg({ quality: 60 })
-        .toFile(outputFilePath, (err, info) => {
-          if (err) {
-            console.error(err);
-          } else {
-            console.log(info);
-          }
-        });
+const inputDirectory = 'src';
+const outputDirectory = 'dist';
+
+const webpOutputQuality = 60; // 0 - 100
+
+function convertImages(directory) {
+
+  fs.readdir(directory, (err, files) => {
+
+    if (err) {
+
+      console.error(err);
+
+      return;
+
     }
+
+    console.log(files);
+
+    files.forEach((file) => {
+
+      const filePath = path.join(directory, file);
+
+      const fileStat = fs.statSync(filePath);
+
+      if (fileStat.isDirectory()) {
+
+        const outputSubdirectory = path.join(directory.replace(inputDirectory, outputDirectory), file);
+
+        if (!fs.existsSync(outputSubdirectory)) {
+
+          fs.mkdirSync(outputSubdirectory, { recursive: true }); // Create directory recursively if it doesn't exist
+
+        }
+
+        convertImages(filePath); // Recursively process subdirectories
+
+      } else if (fileStat.isFile()) {
+
+        // Check if the file is an image (JPEG or PNG) .... can add more formats here if you want??
+
+        if (file.endsWith('.jpg') || file.endsWith('.jpeg') || file.endsWith('.png')) {
+
+          const outputSubdirectory = directory.replace(inputDirectory, outputDirectory);
+
+          const inputFilePath = filePath;
+
+          const fileExtension = path.extname(file);
+
+          const fileNameWithoutExtension = path.basename(file, fileExtension);
+
+          const outputFileName = `${fileNameWithoutExtension}.webp`;
+
+          const outputFilePath = path.join(outputSubdirectory, outputFileName);
+
+          sharp(inputFilePath)
+
+            .webp({ quality: webpOutputQuality }) // Convert to WebP format
+
+            .toFile(outputFilePath, (err, info) => {
+
+              if (err) {
+
+                console.error(err);
+
+              } else {
+
+                console.log(info);
+
+              }
+
+            });
+
+        }
+
+      }
+
+    });
+
   });
-});
+
+}
+
+convertImages(inputDirectory);
